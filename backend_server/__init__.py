f235bf423d4d4f11f8b53dd122d4e5a3f2c03baf
anushkumarv <anush070@gmail.com>
2023-12-11 00:17:32 -0700

    Added minio client

--- backend_server/__init__.py
+++ backend_server/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask import session
+from minio import Minio
 
 from backend_server import constants
 
 app = Flask(__name__)
 app.secret_key = constants.FLASK_SECRET
+
+minio_client = None
+try:
+    minio_client = Minio(constants.minioHost, access_key=constants.minioUser, secret_key=constants.minioPasswd, secure=False)
+except Exception as exp:
+    print(f'Exception raised in while starting minio: {str(exp)}')
 
 from backend_server import auth, routes
--- backend_server/constants.py
+++ backend_server/constants.py
@@ -6,3 +6,8 @@ FLASK_SECRET = os.getenv('FLASK_SECRET') or secrets.token_hex()
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET") 
 GOOGLE_DISCOVERY_URL =  "https://accounts.google.com/.well-known/openid-configuration"
+
+
+minioHost = os.getenv("MINIO_HOST") or "localhost:9000"
+minioUser = os.getenv("MINIO_USER") or "rootuser"
+minioPasswd = os.getenv("MINIO_PASSWD") or "rootpass123"
